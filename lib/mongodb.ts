0382154e3869369ac1408084338bdb9d381f3958
Shaik Subhani <ssk41262@gmail.com>
2026-05-25 21:13:23 +0530

    Fixed monggodb build issue

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,37 +1,46 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error("Please add MONGODB_URI");
-}
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose;
 
 if (!cached) {
+
   cached = (global as any).mongoose = {
     conn: null,
     promise: null,
   };
+
 }
 
 async function connectDB() {
 
+  if (!MONGODB_URI) {
+
+    console.log("MongoDB URI Missing");
+
+    return null;
+
+  }
+
   if (cached.conn) {
+
     return cached.conn;
+
   }
 
   if (!cached.promise) {
 
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI)
+      .then((mongoose) => mongoose);
 
   }
 
   cached.conn = await cached.promise;
 
   return cached.conn;
+
 }
 
 export default connectDB;
